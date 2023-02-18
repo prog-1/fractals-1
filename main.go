432cb4723141acfe3e7f1f8c5ea90e1bb5de372f
Ilya <imolchanov210@gmail.com>
2023-02-18 15:15:33 +0200

    line drawing fix, tracers appear

--- main.go
+++ main.go
@@ -35,11 +35,14 @@ func generate(cnt int, img *ebiten.Image, p1, p2 point, sin, cos float64) *ebite
 	}
 	cnt++
 	fmt.Println(p1, p2)
+
 	np1, np2 := point{p1.x + (p2.x-p1.x)/3, p1.y + (p2.y-p1.y)/3}, point{p2.x - (p2.x-p1.x)/3, p2.y - (p2.y-p1.y)/3}
+	ebitenutil.DrawLine(img, p1.x, p1.y, p2.x, p2.y, c)
+	ebitenutil.DrawLine(img, np1.x, np1.y, np2.x, np2.y, color.Black)
+
 	img = generate(cnt, img, p1, np1, sin, cos)
 	img = generate(cnt, img, np2, p2, sin, cos)
-	ebitenutil.DrawLine(img, p1.x, p1.y, np1.x, np1.y, c)
-	ebitenutil.DrawLine(img, np2.x, np2.y, p2.x, p2.y, c)
+
 	newP2x := ((np2.x-np1.x)*cos - (np2.y-np1.y)*sin) + np1.x
 	newP2y := ((np2.x-np1.x)*sin + (np2.y-np1.y)*cos) + np1.y
 	img = generate(cnt, img, np1, point{newP2x, newP2y}, sin, cos)
