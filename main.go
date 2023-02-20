7c609abc7ae5d77297405234447e618ee8a92ee0
Ričards Šašalevičs <rsasalevics@edu.riga.lv>
2023-02-20 10:21:45 +0200

    .

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"image/color"
 	"log"
 	"math"
@@ -15,42 +14,27 @@ const (
 	Width  = 640
 )
 
-type Line struct {
-	offset_x, offset_y float64
-	rotation           float64
-	length             float64
-}
-
-func (l *Line) Draw(screen *ebiten.Image) {
-	// fmt.Println(l.rotation)
-	x1, y1 := l.offset_x, l.offset_y
-	x2, y2 := (x1+l.length)*math.Cos(l.rotation)-(y1-l.offset_y)*math.Sin(l.rotation)+l.length, (x1)*math.Sin(l.rotation)+(y1-l.offset_y)*math.Cos(l.rotation)+l.offset_y
-	ebitenutil.DrawLine(screen, x1, y1, x2, y2, color.RGBA{255, 0, 0, 255})
-}
-
 // func split(l Line) []Line {
 
 // }
 
 type Game struct {
-	Lines []Line
 }
 
-func split(l Line) (Lines [4]Line) {
-	Lines[0].offset_x = l.offset_x
-	Lines[0].offset_y = l.offset_y
-	Lines[0].length = l.length / 4
+func split(screen *ebiten.Image, x1, y1, x2, y2 float64, depth int) {
+	if depth > 7 {
+		ebitenutil.DrawLine(screen, x1, y1, x2, y2, color.RGBA{255, 0, 0, 255})
+		return
+	}
+	first_x := (x2 - x1) / 3
+	first_y := (y2 - y1) / 3
+	split(screen, x1, y1, first_x+x1, first_y+y1, depth+1)
+	x := (first_x)*math.Cos(math.Pi/3) - (first_y)*math.Sin(math.Pi/3)
+	y := (first_x)*math.Sin(math.Pi/3) + (first_y)*math.Cos(math.Pi/3)
+	split(screen, first_x+x1, first_y+y1, x+x1+first_x, y+y1+first_y, depth+1)
+	split(screen, x+x1+first_x, y+y1+first_y, x2-first_x, y2-first_y, depth+1)
+	split(screen, x2-first_x, y2-first_y, x2, y2, depth+1)
 
-	Lines[1].offset_x = l.offset_x + (l.length / 4)
-	Lines[1].offset_y = l.offset_y
-	Lines[1].length = l.length / 4
-	Lines[1].rotation = l.rotation - math.Pi/3
-
-	// Lines[2].offset_x = l.offset_x + 2*(l.length/10)
-	// Lines[2].offset_y = l.offset_y
-	// Lines[2].length = l.length / 10
-	// Lines[2].rotation = l.rotation + math.Pi/3
-	return
 }
 
 func (g *Game) Update() error {
@@ -58,10 +42,7 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	for _, v := range split(g.Lines[0]) {
-		v.Draw(screen)
-		fmt.Print(v.offset_x)
-	}
+	split(screen, 100, 100, 300, 300, 0)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -70,7 +51,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func main() {
 	ebiten.SetWindowSize(Width, Height)
-	if err := ebiten.RunGame(&Game{Lines: []Line{Line{200, 200, 0, 100}}}); err != nil {
+	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
 }
